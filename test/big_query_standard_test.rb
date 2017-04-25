03f43b7d5d1caaaef033598dd8fbb244f92d5fe8
Shinta Koyanagi <yancya@upec.jp>
2017-04-25 16:50:35 +0900

    Remove Fixnum and Bignum check. For ruby2.4.x (#1)
    hai

--- test/big_query_standard_test.rb
+++ test/big_query_standard_test.rb
@@ -4,65 +4,49 @@ require_relative '../lib/relationizer/big_query/standard.rb'
 class BigQueryStandardTest < Test::Unit::TestCase
   include Relationizer::BigQuery::Standard
 
-  test "BigQuery Standard SQL case 1" do
-    schema = { id: nil, name: nil }
-    tuples = [[1, 'hoge'], [2, 'fuga']]
-    assert_equal(
-      create_relation_literal(schema, tuples),
+  TEST_CASES = {
+    "BigQuery Standard SQL case 1" => [
+      { id: nil, name: nil },
+      [[1, 'hoge'], [2, 'fuga']],
       %Q{SELECT * FROM UNNEST(ARRAY<STRUCT<id INT64, name STRING>>[(1, 'hoge'), (2, 'fuga')])}
-    )
-  end
-
-  test "Array column" do
-    schema = { id: nil, name: nil, combination: nil }
-    tuples = [[1, 'hoge', [1, 2, 3]], [2, 'fuga', [4, 5, 6]]]
-    assert_equal(
-      create_relation_literal(schema, tuples),
+    ],
+    "Array column" => [
+      { id: nil, name: nil, combination: nil },
+      [[1, 'hoge', [1, 2, 3]], [2, 'fuga', [4, 5, 6]]],
       %Q{SELECT * } +
       %Q{FROM UNNEST(ARRAY<STRUCT<id INT64, name STRING, combination ARRAY<INT64>>>} +
       %Q{[(1, 'hoge', [1, 2, 3]), (2, 'fuga', [4, 5, 6])])}
-    )
-  end
-
-  test "FLOAT column" do
-    schema = { id: nil, ratio: nil }
-    tuples = [[1, 1.002], [2, 3.14]]
-    assert_equal(
-      create_relation_literal(schema, tuples),
+    ],
+    "FLOAT column" => [
+      { id: nil, ratio: nil },
+      [[1, 1.002], [2, 3.14]],
       %Q{SELECT * FROM UNNEST(ARRAY<STRUCT<id INT64, ratio FLOAT64>>[(1, 1.002), (2, 3.14)])}
-    )
-  end
-
-  test "DATE column" do
-    schema = { id: nil, birthday: nil }
-    tuples = [[1, Date.parse('1999-02-11')], [2, Date.parse('2000-01-15')]]
-    assert_equal(
-      create_relation_literal(schema, tuples),
+    ],
+    "DATE column" => [
+      { id: nil, birthday: nil },
+      [[1, Date.parse('1999-02-11')], [2, Date.parse('2000-01-15')]],
       %Q{SELECT * FROM UNNEST(ARRAY<STRUCT<id INT64, birthday DATE>>}+
       %Q{[(1, '1999-02-11'), (2, '2000-01-15')])}
-    )
-  end
-
-  test "TIMESTAMP column" do
-    schema = { id: nil, created_at: nil }
-    tuples = [
-      [1, DateTime.parse('1999-02-11 12:00:33')],
-      [2, DateTime.parse('2000-01-15 17:45:11')]
-    ]
-    assert_equal(
-      create_relation_literal(schema, tuples),
+    ],
+    "TIMESTAMP column" => [
+      { id: nil, created_at: nil },
+      [
+        [1, DateTime.parse('1999-02-11 12:00:33')],
+        [2, DateTime.parse('2000-01-15 17:45:11')]
+      ],
       %Q{SELECT * FROM UNNEST(ARRAY<STRUCT<id INT64, created_at TIMESTAMP>>}+
       %Q{[(1, '1999-02-11 12:00:33'), (2, '2000-01-15 17:45:11')])}
-    )
-  end
-
-  test "Single column relation" do
-    schema = { id: nil }
-    tuples = [[1], [2], [3]]
-    assert_equal(
-      create_relation_literal(schema, tuples),
+    ],
+    "Single column relation" => [
+      { id: nil },
+      [[1], [2], [3]],
       %Q{SELECT id FROM UNNEST(ARRAY<STRUCT<id INT64, ___dummy STRING>>} +
       %Q{[(1, NULL), (2, NULL), (3, NULL)])}
-    )
+    ]
+  }
+
+  data(TEST_CASES)
+  test "Test" do |(schema, tuples, expected)|
+    assert_equal(create_relation_literal(schema, tuples), expected)
   end
 end
--- test/postgres_test.rb
+++ test/postgres_test.rb
@@ -1,31 +1,55 @@
 require 'test-unit'
+require 'time'
 require_relative '../lib/relationizer/postgresql.rb'
 
 class PostgresqlTest < Test::Unit::TestCase
   include Relationizer::Postgresql
 
-  # Create relation as
-  #
-  #  id | name
-  # ----+------
-  #   1 | hoge
-  #   2 | fuga
-  #
-  test "Case 1" do
-    schema = { id: nil, name: nil }
-    tuples = [[1, 'hoge'], [2, 'fuga']]
-    assert_equal(
-      create_relation_literal(schema, tuples),
-      %Q{SELECT id::INT8, name::TEXT FROM (VALUES('1', 'hoge'), ('2', 'fuga')) AS t("id", "name")}
-    )
-  end
-
-  test "NULL" do
-    schema = { id: nil }
-    tuples = [[1], [nil]]
-    assert_equal(
-      create_relation_literal(schema, tuples),
+  TEST_CASES = {
+    "NULL" => [
+      { id: nil },
+      [[1], [nil]],
       %Q{SELECT id::INT8 FROM (VALUES('1'), (NULL)) AS t("id")}
-    )
+    ],
+    "INT and TEXT" => [
+      { id: nil, name: nil },
+      [[1, 'hoge'], [2, 'fuga']],
+      %Q{SELECT id::INT8, name::TEXT FROM (VALUES('1', 'hoge'), ('2', 'fuga')) AS t("id", "name")}
+    ],
+    "Bignum" => [
+      { id: nil },
+      [[2_147_483_648]], # 2_147_483_648.bit_length #=> 32
+      %Q{SELECT id::INT8 FROM (VALUES('2147483648')) AS t("id")}
+    ],
+    "Float" => [
+      { f: nil },
+      [[0.0005]],
+      %Q{SELECT f::FLOAT8 FROM (VALUES('0.0005')) AS t("f")}
+    ],
+    "Bool" => [
+      { b: nil },
+      [[true], [false]],
+      %Q{SELECT b::BOOLEAN FROM (VALUES('true'), ('false')) AS t("b")}
+    ],
+    "Date" => [
+      { date: nil },
+      [[Date.parse('2017-04-25')]],
+      %Q{SELECT date::DATE FROM (VALUES('2017-04-25')) AS t("date")}
+    ],
+    "Time" => [
+      { time: nil },
+      [[Time.parse('2017-04-25 16:36:00 00:00:00 +0900')]],
+      %Q{SELECT time::TIMESTAMPTZ FROM (VALUES('2017-04-25 16:36:00 +0900')) AS t("time")}
+    ],
+    "DateTime" => [
+      { time: nil },
+      [[DateTime.parse('2017-04-25 16:36:00 00:00:00 +0900')]],
+      %Q{SELECT time::TIMESTAMPTZ FROM (VALUES('2017-04-25T16:36:00+00:00')) AS t("time")}
+    ]
+  }
+
+  data(TEST_CASES)
+  test "Auto cast test" do |(schema, tuples, expected)|
+    assert_equal(create_relation_literal(schema, tuples), expected)
   end
 end
