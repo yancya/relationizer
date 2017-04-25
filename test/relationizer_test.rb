fab5a8292c1ee4a28fbd3377d6a8cfb877e07a31
Shinta Koyanagi <s_koyanagi@kk-kyowa.co.jp>
2017-04-25 17:34:13 +0900

    Remove useless test

--- test/relationizer_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test-unit'
-require_relative '../lib/relationizer/version.rb'
-
-class RelationizerTest < Test::Unit::TestCase
-  test "version" do
-    assert { Relationizer::VERSION == '0.1.0' }
-  end
-end
