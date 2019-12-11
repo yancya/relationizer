10f42722bf0099cf4070fbb0fc8f9622557778bd
Shinta Koyanagi <yancya@upec.jp>
2019-12-11 18:29:07 +0900

    Fix require path

--- lib/rel_lib/relationizer.rb
+++ lib/rel_lib/relationizer.rb
@@ -1,6 +1,6 @@
 require "relationizer/version"
 require_relative "./relationizer/postgresql.rb"
-require_relative "./relationizer/big_query/Standard.rb"
+require_relative "./relationizer/big_query.rb"
 
 module Relationizer
   # Your code goes here...
